144983c8e06c62e9ca7218dac77347896eac2bc9
EduardoESP <eduardopaulosantana@gmail.com>
2022-05-26 17:32:43 -0300

    Aula 11 pratica de condicionais

---
+++ Aula-11/ex08.js
@@ -0,0 +1,7 @@
+var vel = 40
+console.log(`Avelocidade do seu Carro é ${vel} Km/h`)
+
+if(vel > 60){
+    console.log('Você passou do limite de velocidade permitida, MULTADO!')
+}
+console.log("Dirija sempre usando cinto de segurança")
